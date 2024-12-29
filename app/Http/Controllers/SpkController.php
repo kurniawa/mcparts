2efae6fd7cbb29fae0f540dc463560074b2b63ae
kurniawa <kurniawan_01@ymail.com>
2024-12-29 14:51:24 +0700

    feat/hide SPK akan otomatis hide Nota dan create Nota akan melihat nilai $spk->copy

--- app/Http/Controllers/SpkController.php
+++ app/Http/Controllers/SpkController.php
@@ -703,11 +703,24 @@ class SpkController extends Controller
         $success_ = '';
         $warnings_ = '';
 
+        // dd($spk->notas);
         if ($post['hide'] == 'yes') {
             $spk->copy = false;
+            if (count($spk->notas)) {
+                foreach ($spk->notas as $nota) {
+                    $nota->copy = false;
+                    $nota->save();
+                }
+            }
             $warnings_ .= '-spk hidden-';
         } else {
             $spk->copy = true;
+            if (count($spk->notas)) {
+                foreach ($spk->notas as $nota) {
+                    $nota->copy = true;
+                    $nota->save();
+                }
+            }
             $success_ .= '-spk shown-';
         }
 
